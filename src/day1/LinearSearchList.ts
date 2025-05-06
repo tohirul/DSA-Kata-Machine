d9d1e81522ce71077f7c3aea84d0bc68efa0ec8f
tohirul <tohirul.islam164@gmail.com>
2025-05-06 20:20:41 +0600

    Initial commit for DSA-Kata-Machine

---
+++ src/day1/LinearSearchList.ts
@@ -0,0 +1,8 @@
+export default function linear_search(haystack: number[], needle: number): boolean {
+    for (const item of haystack) {
+        if (item === needle) {
+            return true;
+        }
+    }
+    return false;
+}
