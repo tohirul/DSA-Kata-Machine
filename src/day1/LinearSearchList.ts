ca3a6591098d2b100bf33bebf30846df93dfd917
tohirul <tohirul.islam164@gmail.com>
2025-05-06 21:33:20 +0600

    [Chore] Solved Problems

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,4 +1,5 @@
 export default function linear_search(haystack: number[], needle: number): boolean {
+    
     for (const item of haystack) {
         if (item === needle) {
             return true;
