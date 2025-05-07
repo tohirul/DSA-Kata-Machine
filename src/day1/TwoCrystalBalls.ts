bcf56914a168f8b56e6590e730d433c7b2f7957e
tohirul <tohirul.islam164@gmail.com>
2025-05-07 13:27:59 +0600

    [Chore] Two Crystal Balls problem solved

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // find the sqrt
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    // jump equal to the sqrt untill ball one breaks
+    let i;
+    for (i = jumpAmount; i < breaks.length; i += jumpAmount)
+        // check if ball breaks at each jumping point
+        if (breaks[i]) break;
+
+    // if ball breaks return back to the previous sqrt where the ball did not break
+    i -= jumpAmount;
+
+    // search for breaking point folliwng linear algoithm
+    for (let j = 0; j < jumpAmount && i < breaks.length; j++, i++)
+        // if breaks return the index
+        if (breaks[i]) return i;
+    // return -1 if not found
+    return -1;
 }
